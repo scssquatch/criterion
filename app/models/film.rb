865d10f0c056e71c0de682b2f6d43279003f56c1
Aaron Baker <baker.1833@osu.edu>
2013-05-30 00:04:19 -0400

    bucket. dammit

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,6 @@
 class Film < ActiveRecord::Base
-  has_attached_file :film
-  has_attached_file :preview
+  has_attached_file :film,
+    bucket: "asb-criterion-template-videos"
+  has_attached_file :preview,
+    bucket: "asb-criterion-template-videos"
 end
